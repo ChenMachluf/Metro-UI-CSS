f1fcd15f0221b39482529fea618236bbfdebeb0a
ChenMachluf <chen.mahcluf@gmail.com>
2014-09-16 04:17:45 +0300

    Fix class ".tile" click doesn't work #199
    mouseup didnt raise becuase the animation , mouseleace is called
    instead.
    so , i check in mouseleave handler if the element was animated by
    serching the animation classes.
    if it was animate and the mouse is up (check by listning to the document
    mouseup) then i manualy raise a click event;

--- js/metro-tile-transform.js
+++ js/metro-tile-transform.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function ($) {
     $.widget("metro.tileTransform", {
 
         version: "1.0.0",
@@ -6,60 +6,78 @@
         options: {
         },
 
-        _create: function(){
+        _create: function () {
             var element = this.element;
             var dim = {w: element.width(), h: element.height()};
+            var mouseIsUp = false;
 
-            element.on('click', function(e){
+            function onMouseUp() {
+                mouseIsUp = true;
+                $(document).unbind('mouseup', onMouseUp);
+            }
+
+            element.on('click', function (e) {
 //                e.preventDefault();
 //                if (element[0].tagName == 'A' && element.attr('href')) {
 //                    document.location.href = element.attr('href');
 //                }
             });
 
-            element.on('mousedown', function(e){
+            element.on('mousedown', function (e) {
+                $(document).mouseup(onMouseUp);
+                mouseIsUp = false;
+
                 var X = e.pageX - $(this).offset().left, Y = e.pageY - $(this).offset().top;
                 var transform = 'top';
 
-                if (X < dim.w * 1/3 && (Y < dim.h * 1/2 || Y > dim.h * 1/2 )) {
+                if (X < dim.w * 1 / 3 && (Y < dim.h * 1 / 2 || Y > dim.h * 1 / 2 )) {
                     transform = 'left';
-                } else if (X > dim.w * 2/3 && (Y < dim.h * 1/2 || Y > dim.h * 1/2 )) {
+                } else if (X > dim.w * 2 / 3 && (Y < dim.h * 1 / 2 || Y > dim.h * 1 / 2 )) {
                     transform = 'right'
-                } else if (X > dim.w*1/3 && X<dim.w*2/3 && Y > dim.h/2) {
+                } else if (X > dim.w * 1 / 3 && X < dim.w * 2 / 3 && Y > dim.h / 2) {
                     transform = 'bottom';
                 }
 
-                $(this).addClass("tile-transform-"+transform);
+                $(this).addClass("tile-transform-" + transform);
 
                 if (element[0].tagName == 'A' && element.attr('href')) {
-                    setTimeout(function(){
+                    setTimeout(function () {
                         document.location.href = element.attr('href');
                     }, 500);
                 }
             });
 
-            element.on('mouseup', function(){
+            element.on('mouseup', function () {
                 $(this)
                     .removeClass("tile-transform-left")
                     .removeClass("tile-transform-right")
                     .removeClass("tile-transform-top")
                     .removeClass("tile-transform-bottom");
             });
-            element.on('mouseleave', function(){
+            element.on('mouseleave', function () {
+
+                var fromTransition = $(this).hasClass('tile-transform-left')
+                    || $(this).hasClass('tile-transform-right')
+                    || $(this).hasClass('tile-transform-top')
+                    || $(this).hasClass('tile-transform-bottom');
+
                 $(this)
                     .removeClass("tile-transform-left")
                     .removeClass("tile-transform-right")
                     .removeClass("tile-transform-top")
                     .removeClass("tile-transform-bottom");
+
+                if (fromTransition && mouseIsUp)
+                    $(this).click();
             });
         },
 
-        _destroy: function(){
+        _destroy: function () {
 
         },
 
-        _setOption: function(key, value){
+        _setOption: function (key, value) {
             this._super('_setOption', key, value);
         }
     })
-})( jQuery );
+})(jQuery);
